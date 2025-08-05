129af16085fc1a7e5b8aa508a85aa9f272449b6d
fmbrand <faizulislam837@gmail.com>
2025-08-05 22:24:45 +0600

    add search option scenario

--- features/step-definitions/home.ts
+++ features/step-definitions/home.ts
@@ -1,25 +1,189 @@
-import { Given, When, Then } from "@cucumber/cucumber";
-import { chromium, Browser, Page } from "playwright";
-import { homelocators } from "../../locators/home";
+import { Given, When, Then } from '@cucumber/cucumber';
+import { expect } from '@playwright/test';
+import { Page } from 'playwright';
+import { homeLocators } from '../../locators/home';
+import { CustomWorld } from '../../tests/support/custom-world';
 
-let browser: Browser;
 let page: Page;
 
-Given("I launch the Playwright site",{ timeout: 20000 }, async function () {
-  browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
-  page = await context.newPage();
-  await page.goto("https://playwright.dev");
+Given('Open Browser and Visit Website', async function (this: CustomWorld) {
+  page = this.page;
+  await page.goto(process.env.BIKROY_URL || 'https://bikroy.com/');
 });
 
-When('I search for {string}', async function (searchText: string) {
-  await page.click(homelocators.searchbutton);
-  await page.fill(homelocators.searchInput, searchText);
-  await page.keyboard.press("Enter");
+When('Check that bikroy logo is present', async function () {
+  await expect(page.locator(homeLocators.logo)).toBeVisible();
+});
+Then('Click on search box', async function () {
+  await page.waitForSelector(homeLocators.search_box, { state: 'visible' });
+  await page.click(homeLocators.search_box);
 });
 
-Then("I should see results", async function () {
-  const result = await page.waitForSelector(homelocators.searchResultText);
-  if (!result) throw new Error("No search results found");
-  await browser.close();
+When('Enter text to search option', async function () {
+  await page.fill(homeLocators.search_box, 'iphone');
 });
+
+Then('Click on search option', async function () {
+  await page.click(homeLocators.search);
+});
+
+When('Back to the previous page', async function () {
+  await page.goBack();
+});
+
+// // Generic check function for all logos
+// const checkLogoVisible = (locator: string) => {
+//   return async () => {
+//     await page.waitForSelector(locator, { state: 'visible' });
+//   };
+// };
+
+// When('Check that Mobiles logo is present', checkLogoVisible(homeLocators.mobiles));
+// Then('Check that Electronics logo is present', checkLogoVisible(homeLocators.electrics));
+// Then('Check that Vehicles logo is present', checkLogoVisible(homeLocators.vehicles));
+// Then('Check that Property logo is present', checkLogoVisible(homeLocators.property));
+// Then('Check that Home & Living logo is present', checkLogoVisible(homeLocators.homeLiving));
+// Then('Check that Pets & Animals logo is present', checkLogoVisible(homeLocators.petsAnimals));
+// Then('Check that Mens Fashion & Grooming logo is present', checkLogoVisible(homeLocators.mensFashion));
+// Then('Check that Womens Fashion & Beauty logo is present', checkLogoVisible(homeLocators.womensFashion));
+// Then('Check that Hobbies,Sports & kids logo is present', checkLogoVisible(homeLocators.hobbiesSportsGrooming));
+// Then('Check that business & industry logo is present', checkLogoVisible(homeLocators.businessIndustry));
+// Then('Check that education logo is present', checkLogoVisible(homeLocators.education));
+// Then('Check that Essentials logo is present', checkLogoVisible(homeLocators.essentials));
+// Then('Check that agriculture logo is present', checkLogoVisible(homeLocators.agriculture));
+// Then('Check that Jobs logo is present', checkLogoVisible(homeLocators.jobs));
+// Then('Check that Services logo is present', checkLogoVisible(homeLocators.services));
+// Then('Check that Overseas Jobs logo is present', checkLogoVisible(homeLocators.overseas));
+
+
+// When('Check that login button is present', async () => {
+//   await expect(page.locator(homeLocators.login)).toBeVisible();
+// });
+
+// Then('Click on login button', async () => {
+//   await page.locator(homeLocators.login).click();
+// });
+
+// Then("Click on 'ইমেইল দিয়ে লগ ইন করুন'", async () => {
+//   await page.locator(homeLocators.emaillogin).click();
+// });
+
+// When('Enter valid email', async () => {
+//   await page.locator(homeLocators.emailbox).click();
+//   await page.locator(homeLocators.emailbox).fill('fahim1.qups@gmail.com');
+// });
+
+// When('Enter valid password', async () => {
+//   await page.locator(homeLocators.passwordbox).click();
+//   await page.locator(homeLocators.passwordbox).fill('12345');
+// });
+
+// Then('Click login button', async () => {
+//   await page.locator(homeLocators.loginbutton).click();
+// });
+
+// When('Check that browse items by category is visible', async () => {
+//   await expect(page.locator(homeLocators.category)).toBeVisible();
+// });
+
+// Then('Click on mobiles', async () => {
+//   await page.locator(homeLocators.mobilescategory).click();
+// });
+
+// When('Search mobile model', async () => {
+//   await page.locator(homeLocators.categorysearch_box).fill('Redmi Note 8');
+// });
+
+// Then('Click on search icon', async () => {
+//   await page.locator(homeLocators.categorysearch_button).click();
+// });
+
+// Then('Check that search list is visible', async () => {
+//   const searchList = page.locator(homeLocators.categorysearch_list);
+//   await expect(searchList).toBeVisible();
+//   await expect(searchList).toContainText('Redmi Note 8');
+// });
+
+// Then('Click on electronics', async () => {
+//   await page.locator(homeLocators.electricscategory).click();
+// });
+
+// When('Search  electronics', async () => {
+//   await page.locator(homeLocators.categorysearch_box).fill('Asus vivobook s15');
+// });
+
+// Then('Check that that electrinics list is visible', async () => {
+//   const resultList = page.locator(homeLocators.electricssearchlist);
+//   await expect(resultList).toBeVisible();
+//   await expect(resultList).toContainText('Asus vivobook s15');
+// });
+
+// Then('Click on vehicles', async () => {
+//   await page.locator(homeLocators.vehiclescategory).click();
+// });
+
+// When('Search  vehicles', async () => {
+//   await page.locator(homeLocators.categorysearch_box).fill('Yamaha R15 v3');
+// });
+
+// Then('Check that vehicles list is visible', async () => {
+//   const resultList = page.locator(homeLocators.electricssearchlist);
+//   await expect(resultList).toBeVisible();
+//   await expect(resultList).toContainText('Yamaha R15 v3');
+// });
+
+// Then('Click on Property', async () => {
+//   await page.locator(homeLocators.propertycategory).click();
+// });
+
+// When('Search  Property', async () => {
+//   await page.locator(homeLocators.categorysearch_box).fill('flat');
+// });
+
+// Then('Check that Property list is visible', async () => {
+//   const resultList = page.locator(homeLocators.electricssearchlist);
+//   await expect(resultList).toBeVisible();
+//   await expect(resultList).toContainText('Flat');
+// });
+
+// Then('Click on home & living', async () => {
+//   await page.locator(homeLocators.homeLivingcategory).click();
+// });
+
+// When('Search  home & living', async () => {
+//   await page.locator(homeLocators.categorysearch_box).fill('Tea table');
+// });
+
+// Then('Check that home & living list is visible', async () => {
+//   const resultList = page.locator(homeLocators.electricssearchlist);
+//   await expect(resultList).toBeVisible();
+//   await expect(resultList).toContainText('Tea table');
+// });
+
+// Then('Click on Pets & Animals', async () => {
+//   await page.locator(homeLocators.petsAnimalscategory).click();
+// });
+
+// When('Search  Pets & Animals', async () => {
+//   await page.locator(homeLocators.categorysearch_box).fill('Persian cat');
+// });
+
+// Then('Check that Pets & Animals list is visible', async () => {
+//   const resultList = page.locator(homeLocators.electricssearchlist);
+//   await expect(resultList).toBeVisible();
+//   await expect(resultList).toContainText('Persian cat');
+// });
+
+// Then("Click on Women's Fashion & Beauty", async () => {
+//   await page.locator(homeLocators.womensFashioncategory).click();
+// });
+
+// When("Search  Women's Fashion & Beauty", async () => {
+//   await page.locator(homeLocators.categorysearch_box).fill('3 pis');
+// });
+
+// Then("Check that Women's Fashion & Beauty list is visible", async () => {
+//   const resultList = page.locator(homeLocators.electricssearchlist);
+//   await expect(resultList).toBeVisible();
+//   await expect(resultList).toContainText('3 pis');
+// });
--- locators/home.ts
+++ locators/home.ts
@@ -1,107 +1,40 @@
 export const homeLocators = {
-  // Homepage
+
   logo: "//a[@title='Bikroy - the largest marketplace in Bangladesh']",
   search_box: "//input[@placeholder='What are you looking for?']",
-  search: "//button[@type='submit']",
-
-  // Login
-  username: "//input[@placeholder='Username']",
-  password: "//input[@placeholder='Password']",
-  login: "//button[@class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']",
-
-  // Admin
-  admin: "(//span[normalize-space()='Admin'])[1]",
-  usernameField: "(//input[@class='oxd-input oxd-input--active'])[2]",
-  searchBtn: "//button[@class='oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space']",
-  job: "//span[normalize-space()='Job']",
-  jobTitle: "//a[normalize-space()='Job Titles']",
-  add: "//button[normalize-space()='Add']",
-  jobTitleInput: "//div[@class='oxd-input-group oxd-input-field-bottom-space']//div//input[@class='oxd-input oxd-input--active']",
-  save: "//button[@class='oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space']",
-  delete: "//div[18]//div[1]//div[4]//div[1]//button[1]//i[1]",
-  deleteConfirm: "//button[normalize-space()='Yes, Delete']",
-
-  // Organization > General Info
-  organization: "//span[normalize-space()='Organization']",
-  generalInfo: "//a[normalize-space()='General Information']",
-  editSwitch: "//span[@class='oxd-switch-input oxd-switch-input--active --label-left']",
-  orgName: "(//input[@class='oxd-input oxd-input--active'])[2]",
-  regNumber: "(//input[@class='oxd-input oxd-input--active'])[3]",
-  taxId: "(//input[@class='oxd-input oxd-input--active'])[4]",
-  phone: "(//input[@class='oxd-input oxd-input--active'])[5]",
-  email: "(//input[@class='oxd-input oxd-input--active'])[6]",
-  saveGeneralInfo: "(//button[normalize-space()='Save'])[1]",
-
-  // Organization > Locations
-  locationsTab: "(//a[normalize-space()='Locations'])[1]",
-  locationName: "(//input[@class='oxd-input oxd-input--active'])[2]",
-  locationSearchBtn: "(//button[normalize-space()='Search'])[1]",
-
-  // Organization > Structure
-  structureTab: "(//a[normalize-space()='Structure'])[1]",
-  structureEditSwitch: "(//span[@class='oxd-switch-input oxd-switch-input--active --label-left'])[1]",
-  addStructure: "(//button[normalize-space()='Add'])[1]",
-  structureName: "((//input[@class='oxd-input oxd-input--active'])[3])",
-  saveStructure: "(//button[normalize-space()='Save'])[1]",
-
-  // Qualifications > Skills
-  qualificationsTab: "(//span[normalize-space()='Qualifications'])[1]",
-  skillsTab: "(//a[normalize-space()='Skills'])[1]",
-  addSkill: "(//button[normalize-space()='Add'])[1]",
-  skillNameField: "(//div[@class='oxd-form-row'])[1]",
-  saveSkill: "(//button[normalize-space()='Save'])[1]",
-
-  // Qualifications > Education
-  educationTab: "(//a[@class='oxd-topbar-body-nav-tab-link'])[2]",
-  addEducation: "(//button[normalize-space()='Add'])[1]",
-  educationLevel: "(//input[@class='oxd-input oxd-input--active'])[2]",
-  saveEducation: "(//button[normalize-space()='Save'])[1]",
-
-  // Qualifications > Languages
-  languagesTab: "(//a[normalize-space()='Languages'])[1]",
-  addLanguage: "(//button[normalize-space()='Add'])[1]",
-  languageName: "(//input[@class='oxd-input oxd-input--active'])[2]",
-  saveLanguage: "(//button[normalize-space()='Save'])[1]",
-
-  // Qualifications > Memberships
-  membershipsTab: "(//a[@class='oxd-topbar-body-nav-tab-link'])[5]",
-  addMembership: "(//button[normalize-space()='Add'])[1]",
-  membershipName: "(//input[@class='oxd-input oxd-input--active'])[2]",
-  saveMembership: "(//button[normalize-space()='Save'])[1]",
-
-  // Nationalities
-  nationalitiesTab: "(//a[normalize-space()='Nationalities'])[1]",
-  addNationality: "//button[@class='oxd-button oxd-button--medium oxd-button--secondary']",
-  nationalityName: "(//input[@class='oxd-input oxd-input--active'])[2]",
-  saveNationality: "(//button[normalize-space()='Save'])[1]",
-
-  // Corporate Branding
-  corpBrandingTab: "(//a[normalize-space()='Corporate Branding'])[1]",
-  colorPicker: "(//div[@class='oxd-color-input-preview'])[4]",
-  colorSelector: "(//div[@class='oxd-color-picker-indicator'])[1]",
-  publishBtn: "(//button[normalize-space()='Publish'])[1]",
-
-  // Configuration > Email Config
-  configurationTab: "(//span[normalize-space()='Configuration'])[1]",
-  emailConfigTab: "(//a[@class='oxd-topbar-body-nav-tab-link'])[1]",
-  emailInput: "(//input[@class='oxd-input oxd-input--active'])[2]",
-  saveEmailConfig: "(//button[normalize-space()='Save'])[1]",
-
-  // Configuration > Email Subscriptions
-  emailSubsTab: "(//a[normalize-space()='Email Subscriptions'])[1]",
-  addApplicantIcon: "(//i[@class='oxd-icon bi-person-plus-fill'])[1]",
-  addApplicantBtn: "(//button[normalize-space()='Add'])[1]",
-  subscriberName: "(//input[@class='oxd-input oxd-input--active'])[2]",
-  subscriberEmail: "(//div//input[@class='oxd-input oxd-input--active'])[3]",
-  saveSubscription: "(//button[normalize-space()='Save'])[1]",
-
-  // Configuration > Modules
-  modulesTab: "(//a[normalize-space()='Modules'])[1]",
-  mobileToggle: "(//span[@class='oxd-switch-input oxd-switch-input--active --label-right'])[9]",
-  saveModules: "(//button[normalize-space()='Save'])[1]",
-
-  // PIM section
-  pimTab: "(//a[@class='oxd-main-menu-item'])[1]",
-  pimSearchBtn: "//button[@class='oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space']",
-  pimResetBtn: "//button[@class='oxd-button oxd-button--medium oxd-button--ghost']"
-};
+  search:  "//button[@type='submit']",
+  mobiles:  "//p[normalize-space()='Mobiles']",
+  electrics:  "//p[normalize-space()='Electronics']",
+  vehicles:  "//p[normalize-space()='Vehicles']",
+  property:  "//p[normalize-space()='Property']",
+  homeLiving:  "//p[normalize-space()='Home & Living']",
+  petsAnimals:  "//p[normalize-space()='Pets & Animals']",
+  mensFashion:  "//a[@href='/en/ads/bangladesh/mens-fashion']//div[@class='info--uF0qH']//p[@class='info-title--3CkVD']",
+  womensFashion:  "//a[@href='/en/ads/bangladesh/womens-fashion']//div[@class='info--uF0qH']//p[@class='info-title--3CkVD']",
+  hobbiesSportsGrooming:  "//p[normalize-space()='Hobbies, Sports & Kids']",
+  businessIndustry:  "//p[normalize-space()='Business & Industry']",
+  education:  "//p[normalize-space()='Education']",
+  essentials:  "//p[normalize-space()='Essentials']",
+  agriculture:  "//p[normalize-space()='Agriculture']",
+  jobs:  "//p[normalize-space()='Jobs']",
+  services:  "//p[normalize-space()='Services']",
+  overseas:  "//p[normalize-space()='Overseas Jobs']",
+  login:  "//span[@class='title--1NHWk gtm-login-click']",
+  emaillogin:  "//span[@class='email-text--FbASN']",
+  emailbox:  "//input[@id='input_email']",
+  passwordbox:  "//input[@id='input_password']",
+  loginbutton:  "//button[normalize-space()='Login']",
+  category:  "//h2[normalize-space()='Browse items by category']",
+  mobilescategory:  "//p[normalize-space()='Mobiles']",
+  categorysearch_box:  "//input[@placeholder='What are you looking for?']",
+  categorysearch_button:  "//button[@type='submit']",
+  categorysearch_list:  '//*[@id="app-wrapper"]/div[1]/div[3]/div/div[2]/div[3]/div[1]/div[1]/div/div/ol/li[4]/a/div',
+  electricscategory:  "//p[normalize-space()='Electronics']",
+  electricssearchlist:  "//div[@class='last-link--qlz8S link-text--1Tj-x']",
+  vehiclescategory:  "//p[normalize-space()='Vehicles']",
+  propertycategory:  "//p[normalize-space()='Property']",
+  homeLivingcategory:  "//p[normalize-space()='Home & Living']",
+  petsAnimalscategory:  "//p[normalize-space()='Pets & Animals']",
+  mensFashioncategory:  "//p[normalize-space()='Mens Fashion']",
+  womensFashioncategory:  "//a[@href='/en/ads/bangladesh/womens-fashion']//div[@class='info--uF0qH']//p[@class='info-title--3CkVD']",
+}
